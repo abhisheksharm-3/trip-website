68ac53b1f636a9b89c8c58e82b702b2a1692ccdf
Abhishek <abhitiku2003@gmail.com>
2023-09-17 13:55:01 +0530

    fixed overflow

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -5,7 +5,7 @@ import HeaderMain from "@/app/components/HeaderMain/page";
 
 const LandingPage = () => {
   return (
-    <div className="bg-[url('/images/homepage-main.jpeg')] bg-cover bg-no-repeat lg:bg-center">
+    <div className="bg-[url('/images/homepage-main.jpeg')] bg-cover bg-no-repeat lg:bg-center overflow-x-hidden">
       <Navbar />
       <HeaderMain />
     </div>
