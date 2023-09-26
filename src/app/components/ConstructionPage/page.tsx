06b595dd7a582276af13ed4831a23eec4f31f3dc
Abhishek <abhitiku2003@gmail.com>
2023-09-26 12:29:52 +0530

    profile page

--- src/app/components/ConstructionPage/page.tsx
+++ src/app/components/ConstructionPage/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "../Navbar/page";
 import Image from "next/image";
 import construction from "../../../../public/images/construction.svg";
+import NavbarLoggedIn from "../Navbar-LoggedIn/page";
 
 const ConstructionPage = () => {
   return (
     <div className="bg-[#090909] overflow-x-hidden">
-      <Navbar />
+      <NavbarLoggedIn />
       <div className=" flex flex-col w-screen items-center justify-center h-screen text-[#acacac]">
         <Image src={construction} alt="construction" />
         <p className="w-[439px] h-[72px] text-[26px] font-extrabold leading-9">
