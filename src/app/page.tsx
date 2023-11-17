e514b997b10dd87bd6debff75480ec36f76b1812
Abhishek <abhitiku2003@gmail.com>
2023-11-17 18:26:42 +0530

    minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import LandingPage from "./landing/page"
 import { Metadata } from 'next'
 import {NextUIProvider} from "@nextui-org/react";
  
-export const metadata: Metadata = {
-  title: 'TripTalkies',
-}
+// export const metadata: Metadata = {
+//   title: 'TripTalkies',
+// }
 
 export default function Home() {
   return (
